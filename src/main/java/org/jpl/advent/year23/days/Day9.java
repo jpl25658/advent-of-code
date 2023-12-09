3bf7d853aad799fae2aeb045a70e1dbe33dab4f3
jpl <jpl25658@gmail.com>
2023-12-09 09:18:28 +0100

    day 9 alternate solution
    extrapolateBackward is equivalent to do extrapolateForward to the inverse sequence

--- src/main/java/org/jpl/advent/year23/days/Day9.java
+++ src/main/java/org/jpl/advent/year23/days/Day9.java
@@ -4,6 +4,7 @@ import org.jpl.advent.year23.Day2023;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,43 +21,38 @@ public class Day9 extends Day2023 {
 
   @Override
   public Object part1() {
-    return dayStream().map(this::listOfInteger).mapToInt(this::extrapolateForward).sum();
+    return dayStream().map(Day9::listOfInteger)
+        .mapToInt(Day9::extrapolate)
+        .sum();
   }
 
   @Override
   public Object part2() {
-    return dayStream().map(this::listOfInteger).mapToInt(this::extrapolateBackward).sum();
+    return dayStream().map(Day9::listOfInteger)
+        .peek(Collections::reverse)
+        .mapToInt(Day9::extrapolate)
+        .sum();
   }
 
-  private List<Integer> listOfInteger(String input) {
-    return Arrays.stream(input.split("\\s+")).map(Integer::valueOf).toList();
+  private static List<Integer> listOfInteger(String input) {
+    return Arrays.stream(input.split("\\s+")).map(Integer::valueOf).collect(Collectors.toList());
   }
 
-  private Integer extrapolateForward(List<Integer> list) {
-    List<Integer> lastValues = new ArrayList<>();
+  private static Integer extrapolate(List<Integer> list) {
+    List<Integer> values = new ArrayList<>();
     LinkedList<Integer> actual = new LinkedList<>(list);
     while (actual.stream().anyMatch(n -> n != 0)) {
-      lastValues.add(actual.getLast());
+      values.add(actual.getLast());
       actual = new LinkedList<>(calcDifferences(actual));
     }
-    return lastValues.stream().mapToInt(Integer::intValue).sum();
+    return values.stream().mapToInt(Integer::intValue).sum();
   }
 
-  private LinkedList<Integer> calcDifferences(List<Integer> list) {
+  private static LinkedList<Integer> calcDifferences(List<Integer> list) {
     return IntStream.range(1, list.size())
-        .mapToObj(i -> list.get(i) - list.get(i - 1))
+        .map(i -> list.get(i) - list.get(i - 1))
+        .boxed()
         .collect(Collectors.toCollection(LinkedList::new));
-  }
-
-  private Integer extrapolateBackward(List<Integer> list) {
-    LinkedList<Integer> firstValues = new LinkedList<>();
-    LinkedList<Integer> actual = new LinkedList<>(list);
-    while (actual.stream().anyMatch(n -> n != 0)) {
-      firstValues.push(actual.getFirst());
-      actual = new LinkedList<>(calcDifferences(actual));
-    }
-
-    return firstValues.stream().reduce(0, (a, n) -> n - a);
   }
 
 }
